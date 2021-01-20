b3f3b74aac69673fe59615742dcc89df0516e3a5
Kadenza44 <iliassin@yandex.ru>
2021-01-20 11:46:10 +0300

    Added Exeption

--- main.py
+++ main.py
@@ -19,6 +19,49 @@ class ConvertionException(Exception):
     pass
 
 
+class MoneyConvertion:
+
+    @staticmethod
+    def convertion(text_user):
+        if len(text_user) != 3:
+            print('Неверное количество параметров')
+            return 'Неверное количество параметров'
+            # raise ConvertionException('Неверное количество параметров')
+        quote = text_user[0].lower()
+        base = text_user[1].lower()
+
+        if quote == base:
+            print(f'Перевод двух одинаковых валют {quote} недопустим')
+            return f'Перевод двух одинаковых валют {quote} недопустим'
+            # raise ConvertionException(f'Перевод двух одинаковых валют {quote} недопустим')
+
+        try:
+            amount = float(text_user[2])
+        except ValueError:
+            print(f'неверно задано количество ({text_user[2]})')
+            return f'неверно задано количество ({text_user[2]})'
+            # raise ConvertionException(f'неверно задано количество ({text_user[2]})')
+
+        try:
+            quote_ticker = values_key[quote]
+        except KeyError:
+            print(f'Валюты {quote} нет в списке доступных')
+            return f'Валюты {quote} нет в списке доступных'
+            # raise ConvertionException(f'Валюты {quote} нет в списке доступных')
+
+        try:
+            base_ticker = values_key[base]
+        except KeyError:
+            print(f'Валюты {base} нет в списке доступных')
+            return f'Валюты {base} нет в списке доступных'
+            # raise ConvertionException(f'Валюты {base} нет в списке доступных')
+
+        r = requests.get(f'https://api.exchangeratesapi.io/latest?base={quote_ticker}&symbols={base_ticker}')
+        rate = json.loads(r.content)['rates'][base_ticker]
+        text = f'Курс {amount} {quote_ticker} к {base_ticker} равен {rate * amount}'
+        return text
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     text = 'Для конвертации валют введите команду в следующем формате:\n\
@@ -38,11 +81,13 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def convert(message: telebot.types.Message):
-    quote, base, amount = message.text.split(' ')
-    r = requests.get(f'https://api.exchangeratesapi.io/latest?base={values_key[quote]}&symbols={values_key[base]}')
-    rate = json.loads(r.content)['rates'][values_key[base]]
-    text = f'Курс {amount} {values_key[quote]} к {values_key[base]} равен {rate * float(amount)}'
-    bot.send_message(message.chat.id, text)
+    text_user = message.text.split(' ')
+    try:
+        text = MoneyConvertion.convertion(text_user)
+    except:
+        bot.send_message(message.chat.id, 'Что-то пошло не так')
+    else:
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling()
